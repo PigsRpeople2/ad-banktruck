0659eec712000195da7a89863be2aa642d119df3
PigsRpeople2 <167029562+PigsRpeople2@users.noreply.github.com>
2026-02-10 14:49:44 +1100

    Basic Structure

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'PigsRpeople2'
+description 'An advanced bank truck robbery script'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+lua54 'yes'
+
+dependencies {
+    'ox_lib'
+}
